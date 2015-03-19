5c8c7e0ad1cb43f34876a51a4857af7a83a6bedd
Rowan Miller <mail@romiller.com>
2015-03-19 12:43:02 -0700

    :star: Basic product and category browsing

---
+++ UnicornStore.AspNet/src/UnicornStore.AspNet/ViewModels/Shop/CategoryViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using UnicornStore.AspNet.Models.UnicornStore;
+
+namespace UnicornStore.AspNet.ViewModels.Shop
+{
+    public class CategoryViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public Category Category { get; set; }
+        public IEnumerable<CategoryInfo> ParentHierarchy { get; set; }
+        public IEnumerable<CategoryInfo> Children { get; set; }
+    }
+}
