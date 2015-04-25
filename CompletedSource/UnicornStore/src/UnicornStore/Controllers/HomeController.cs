48c9dd6a5709a3cd6861a59171cdb1d31c1fe8e1
Rowan Miller <mail@romiller.com>
2015-04-25 10:55:02 -0700

    :wrench: Move apps to CompletedSource folder

---
+++ CompletedSource/UnicornStore/src/UnicornStore/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.Mvc;
+using UnicornStore.AspNet.Models.UnicornStore;
+using UnicornStore.AspNet.ViewModels.Home;
+
+namespace UnicornStore.AspNet.Controllers
+{
+    public class HomeController : Controller
+    {
+        private UnicornStoreContext db;
+
+        public HomeController(UnicornStoreContext context)
+        {
+            db = context;
+        }
+
+        public IActionResult Index() 
+        {
+            var ads = db.WebsiteAds
+                .Where(a => a.Start == null || a.Start <= DateTime.Now.ToUniversalTime())
+                .Where(a => a.End == null || a.End >= DateTime.Now.ToUniversalTime());
+
+            return View(new IndexViewModel
+            {
+                TopLevelCategories = ShopController.GetTopLevelCategories(db).ToList(),
+                CurrentAds = ads.ToList()
+            });
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View("~/Views/Shared/Error.cshtml");
+        }
+    }
+}
---
+++ CompletedSource/UnicornStore/src/UnicornStore/Startup.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Diagnostics;
+using Microsoft.AspNet.Diagnostics.Entity;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Data.Entity;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.Logging;
+using UnicornStore.AspNet.Models.Identity;
+using UnicornStore.AspNet.Models.UnicornStore;
+
+namespace UnicornStore.AspNet
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            // Setup configuration sources.
+            Configuration = new Configuration()
+                .AddJsonFile("config.json")
+                .AddJsonFile("secrets.json")
+                .AddEnvironmentVariables();
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add EF services to the services container.
+            services.AddEntityFramework()
+                .AddSqlServer()
+                .AddDbContext<UnicornStoreContext>(options => options.UseSqlServer(Configuration.Get("ConnectionStrings:UnicornStore")))
+                .AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.Get("ConnectionStrings:UnicornStore")));
+
+            // Add Identity services to the services container.
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            services.ConfigureFacebookAuthentication(options =>
+            {
+                options.AppId = "1593240960890768";
+                options.AppSecret = Configuration.Get("secrets:facebook:appSecret");
+            });
+
+            services.ConfigureGoogleAuthentication(options =>
+            {
+                options.ClientId = "140672572048-92ggg4tb5ihr7ffats86pk4cgecg0cn4.apps.googleusercontent.com";
+                options.ClientSecret = Configuration.Get("secrets:google:clientSecret");
+            });
+
+            // Add MVC services to the services container.
+            services.AddMvc();
+
+            // Uncomment the following line to add Web API servcies which makes it easier to port Web API 2 controllers.
+            // You need to add Microsoft.AspNet.Mvc.WebApiCompatShim package to project.json
+            // services.AddWebApiConventions();
+
+        }
+
+        // Configure is called after ConfigureServices is called.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerfactory)
+        {
+            loggerfactory.AddConsole();
+
+            // Add the following to the request pipeline only in development environment.
+            if (string.Equals(env.EnvironmentName, "Development", StringComparison.OrdinalIgnoreCase))
+            {
+                app.UseErrorPage(ErrorPageOptions.ShowAll);
+                app.UseDatabaseErrorPage(DatabaseErrorPageOptions.ShowAll);
+                app.EnsureSampleData();
+            }
+            else
+            {
+                // Add Error handling middleware which catches all application specific errors and
+                // send the request to the following path or controller action.
+                app.UseErrorHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseIdentity();
+            app.UseFacebookAuthentication();
+            app.UseGoogleAuthentication();
+            app.EnsureRolesCreated();
+            app.ProcessPreApprovedAdmin(Configuration.Get("secrets:preApprovedAdmin"));
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
+        }
+    }
+}
