b545dc66ba32bf575ad7b6eb976e49147ee93816
Rowan Miller <mail@romiller.com>
2015-04-27 11:22:01 -0700

    :arrow_up: Upgrade to Beta4 project system

--- CompletedSource/UnicornStore/src/UnicornStore/Startup.cs
+++ CompletedSource/UnicornStore/src/UnicornStore/Startup.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Microsoft.AspNet.Builder;
+﻿using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Diagnostics;
 using Microsoft.AspNet.Diagnostics.Entity;
 using Microsoft.AspNet.Hosting;
@@ -11,33 +10,47 @@ using Microsoft.Framework.Logging;
 using UnicornStore.AspNet.Models.Identity;
 using UnicornStore.AspNet.Models.UnicornStore;
 
-namespace UnicornStore.AspNet
+namespace UnicornStore
 {
     public class Startup
     {
         public Startup(IHostingEnvironment env)
         {
             // Setup configuration sources.
-            Configuration = new Configuration()
+            var configuration = new Configuration()
                 .AddJsonFile("config.json")
                 .AddJsonFile("secrets.json")
-                .AddEnvironmentVariables();
+                .AddJsonFile($"config.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsEnvironment("Development"))
+            {
+                // This reads the configuration keys from the secret store.
+                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                configuration.AddUserSecrets();
+            }
+
+            configuration.AddEnvironmentVariables();
+            Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; set; }
 
-        // This method gets called by the runtime.
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add Application settings to the services container.
+            services.Configure<AppSettings>(Configuration.GetSubKey("AppSettings"));
+
             // Add EF services to the services container.
             services.AddEntityFramework()
                 .AddSqlServer()
-                .AddDbContext<UnicornStoreContext>(options => options.UseSqlServer(Configuration.Get("ConnectionStrings:UnicornStore")))
-                .AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.Get("ConnectionStrings:UnicornStore")));
+                .AddDbContext<UnicornStoreContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:UnicornStore"]))
+                .AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:UnicornStore"]));
 
             // Add Identity services to the services container.
             services.AddIdentity<ApplicationUser, IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
 
             services.ConfigureFacebookAuthentication(options =>
             {
@@ -54,20 +67,23 @@ namespace UnicornStore.AspNet
             // Add MVC services to the services container.
             services.AddMvc();
 
-            // Uncomment the following line to add Web API servcies which makes it easier to port Web API 2 controllers.
-            // You need to add Microsoft.AspNet.Mvc.WebApiCompatShim package to project.json
+            // Uncomment the following line to add Web API services which makes it easier to port Web API 2 controllers.
+            // You will also need to add the Microsoft.AspNet.Mvc.WebApiCompatShim package to the 'dependencies' section of project.json.
             // services.AddWebApiConventions();
-
         }
 
         // Configure is called after ConfigureServices is called.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerfactory)
         {
-            loggerfactory.AddConsole();
+            // Configure the HTTP request pipeline.
+
+            // Add the console logger.
+            loggerfactory.AddConsole(minLevel: LogLevel.Warning);
 
             // Add the following to the request pipeline only in development environment.
-            if (string.Equals(env.EnvironmentName, "Development", StringComparison.OrdinalIgnoreCase))
+            if (env.IsEnvironment("Development"))
             {
+                app.UseBrowserLink();
                 app.UseErrorPage(ErrorPageOptions.ShowAll);
                 app.UseDatabaseErrorPage(DatabaseErrorPageOptions.ShowAll);
                 app.EnsureMigrationsApplied();
@@ -76,24 +92,30 @@ namespace UnicornStore.AspNet
             else
             {
                 // Add Error handling middleware which catches all application specific errors and
-                // send the request to the following path or controller action.
+                // sends the request to the following path or controller action.
                 app.UseErrorHandler("/Home/Error");
             }
 
+            // Add static files to the request pipeline.
             app.UseStaticFiles();
 
+            // Add cookie-based authentication to the request pipeline.
             app.UseIdentity();
             app.UseFacebookAuthentication();
             app.UseGoogleAuthentication();
             app.EnsureRolesCreated();
             app.ProcessPreApprovedAdmin(Configuration.Get("secrets:preApprovedAdmin"));
 
+            // Add MVC to the request pipeline.
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
                     template: "{controller}/{action}/{id?}",
                     defaults: new { controller = "Home", action = "Index" });
+
+                // Uncomment the following line to add a route for porting Web API 2 controllers.
+                // routes.MapWebApiRoute("DefaultApi", "api/{controller}/{id?}");
             });
         }
     }
